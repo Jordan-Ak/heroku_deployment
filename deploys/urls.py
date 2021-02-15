6ea8d62589a51e9cccb8f56855d0fd18857f2149
Jordan Akpoborie <JosiahDavid98@gmail.com>
2021-02-15 19:22:52 +0100

    Have a fully functional site on local server.

---
+++ deploys/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import HomePageView, AboutPageView
+
+app_name = 'deploys'
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name = 'home'),
+    path('about/', AboutPageView.as_view(), name = 'about'),
+]
